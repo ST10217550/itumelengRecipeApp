4b4cf764120dcb5b6b26e39cc9ee9876f04fe877
Itumeleng Shuping <ST10217550@rcconnect.edu.za>
2023-04-19 11:33:12 +0000

    added a class

--- itumelengRecipeApp/Program.cs
+++ itumelengRecipeApp/Program.cs
@@ -10,6 +10,7 @@ namespace itumelengRecipeApp
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
         }
     }
 }
---
+++ itumelengRecipeApp/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itumelengRecipeApp
+{
+    internal class Recipe
+    {
+        private int numOfIng;
+        private String nameOfRecipe;
+
+
+
+        Console(writeline);
+        
+
+        for(int i; i <numOfIng; i++)
+
+    }
+
+    internal class pritave
+    {
+    }
+}
